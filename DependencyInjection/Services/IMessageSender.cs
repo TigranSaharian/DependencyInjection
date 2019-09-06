1f29819df5897b0765f437a00e8cc94f6fd6989c
TigranSaharian <TigranSaharian@users.noreply.github.com>
2019-09-06 14:39:14 +0400

    Add project files.

---
+++ DependencyInjection/Services/IMessageSender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Services
+{
+    public interface IMessageSender
+    {
+        string Send();
+    }
+
+    public class EmailMessageSender : IMessageSender
+    {
+        public string Send()
+        {
+            return "The message was sent by Email";
+        }
+    }
+
+    public class SmsMessageSender : IMessageSender
+    {
+        public string Send()
+        {
+            return "Message was sent by SMS";
+        }
+    }
+}
